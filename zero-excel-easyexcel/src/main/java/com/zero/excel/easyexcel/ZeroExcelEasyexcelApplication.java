01e0e3f710cd833ffa1569bb9b678a1e0a778d48
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-27 14:35:12 +0800

    feat: springboot 集成 easyexcel

---
+++ zero-excel-easyexcel/src/main/java/com/zero/excel/easyexcel/ZeroExcelEasyexcelApplication.java
@@ -0,0 +1,13 @@
+package com.zero.excel.easyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroExcelEasyexcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroExcelEasyexcelApplication.class, args);
+    }
+
+}
